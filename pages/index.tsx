80cff2ac3f6a43664f93bfc05efbe30a012a0617
JacobG03 <jacobgluszek03@gmail.com>
2022-07-03 18:01:39 +0200

    tailwindcss installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='h-16 bg-blue-400'>
+      <h1 className='text-indigo-50'>BLUE</h1>
     </div>
   )
 }
