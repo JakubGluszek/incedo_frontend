a53ef19882e64ea600f67d68212c23fb4d477b9c
JacobG03 <jacobgluszek03@gmail.com>
2022-07-03 18:21:17 +0200

    dailyui installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='h-16 bg-blue-400'>
-      <h1 className='text-indigo-50'>BLUE</h1>
+    <div className='prose lg:prose-xl'>
+      <h1 className='font-semibold'>BLUE</h1>
+      <h3>suuuub</h3>
+      <p>longer text</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
+  //darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  }
 }
